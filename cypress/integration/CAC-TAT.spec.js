d64404b6d3b4473fe09c5f998ba5455c0cb6c6ad
Victor Ribeiro <ribeiro.victor@outlook.com>
2024-11-07 14:28:22 -0300

    fim

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -234,7 +234,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         })
     })
 
-    it.only('encontra o gato escondido', function() {
+    it('encontra o gato escondido', function() {
       cy.get('#cat')
         .invoke('show')
         .should('be.visible')
--- cypress/integration/privacy.js
+++ cypress/integration/privacy.js
@@ -1,5 +1,5 @@
 Cypress._.times(5, function() {
-  it.only('testa a página da política de privacidade de forma independente', function() {
+  it('testa a página da política de privacidade de forma independente', function() {
     cy.visit('./src/privacy.html')
 
     cy.get('#title')
